4ea82c108f83054c831f6a8d24dc91f85a070611
svenrr <67638900+svenrr@users.noreply.github.com>
2020-11-09 18:35:34 +0100

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -13,13 +13,11 @@ st.file_uploader('File uploader', key="s")
 
 if st.button("Magic"): 
   my_bar = st.progress(0)
-  for percent_complete in range(100):
-      time.sleep(0.1)
-      my_bar.progress(percent_complete + 1)
-      
   st.spinner()
   with st.spinner(text='In progress'):
-    time.sleep(5)
+    for percent_complete in range(100):
+        time.sleep(0.1)
+        my_bar.progress(percent_complete + 1)
   st.success('Done')
 
 st.write("### Output")
