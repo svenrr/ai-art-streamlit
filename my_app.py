8b6da6439764b13274d40c8d5ce7d84576bf8eae
svenrr <67638900+svenrr@users.noreply.github.com>
2020-11-09 18:34:07 +0100

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -15,7 +15,7 @@ if st.button("Magic"):
   my_bar = st.progress(0)
   for percent_complete in range(100):
       time.sleep(0.1)
-      my_bar.progress(percent_complete + 1))
+      my_bar.progress(percent_complete + 1)
       
   st.spinner()
   with st.spinner(text='In progress'):
