44bd70477e54b84c521e1af2383b2ce40580cca5
svenrr <67638900+svenrr@users.noreply.github.com>
2020-11-09 18:33:29 +0100

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import time
 
 st.write("# AI Art") 
 
@@ -11,10 +12,14 @@ st.write("### Style Image")
 st.file_uploader('File uploader', key="s")
 
 if st.button("Magic"): 
-  st.progress(progress_variable_1_to_100)
+  my_bar = st.progress(0)
+  for percent_complete in range(100):
+      time.sleep(0.1)
+      my_bar.progress(percent_complete + 1))
+      
   st.spinner()
   with st.spinner(text='In progress'):
     time.sleep(5)
-    st.success('Done')
+  st.success('Done')
 
 st.write("### Output")
