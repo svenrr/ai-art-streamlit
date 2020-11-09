1ac972404b108436f6476331cea071766dfb1092
svenrr <67638900+svenrr@users.noreply.github.com>
2020-11-09 18:28:57 +0100

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -2,4 +2,19 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+st.write("# AI Art") 
+
+st.write("### Content Image")
 st.file_uploader('File uploader')
+
+st.write("### Style Image")
+st.file_uploader('File uploader')
+
+if st.button("Magic"): 
+  st.progress(progress_variable_1_to_100)
+  st.spinner()
+  with st.spinner(text='In progress'):
+  time.sleep(5)
+  st.success('Done')
+
+st.write("### Output")
